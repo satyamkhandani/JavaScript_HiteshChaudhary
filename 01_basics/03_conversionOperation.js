2f70bdd2339e920bc610cc2ee50f6ab54c9096f5
satyamkhandani <thesatyamkhandani@gmail.com>
2025-11-05 09:32:22 +0000

    operators

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -28,3 +28,66 @@ let someNumber = 33
 let stringNumber = String (someNumber)
 console.log(stringNumber)
 console.log(typeof stringNumber)
+
+
+
+//   operators ................................
+
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**2)
+// console.log(2/2)
+// console.log(2%3)
+
+let str1 = "hello"
+let str2 =  " Satyam"
+
+let str3 = str1 + str2 ;
+
+
+console.log("1" + 2);       //12
+console.log(1 + "2");       //12
+console.log("1" + 2 + 2);   //122
+console.log(1 + 2 + "2");   //32
+
+
+console.log(true);      //true
+console.log(+true);    // 1
+console.log(true+);   // error
+
+
+ console.log(+"") 
+ 
+ 
+
+
+ let num1 , num2 , num3
+
+ num1 = num2 = num3 = 2 + 2
+
+ let gameCounter = 100
+ gameCounter++;
+
+console.log(gameCounter)
+
+
+// Postfix increment.................
+let x = 3;
+const y = x++;
+// x is 4; y is 3
+
+let x2 = 3n;
+const y2 = x2++;
+// x2 is 4n; y2 is 3n
+
+
+// Prefix increment....................
+let x = 3;
+const y = ++x;
+// x is 4; y is 4
+
+let x2 = 3n;
+const y2 = ++x2;
+// x2 is 4n; y2 is 4n
