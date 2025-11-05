99b312d1e06d2c400a4c45e25d3b28275a1e8f06
satyamkhandani <thesatyamkhandani@gmail.com>
2025-11-05 07:23:55 +0000

    datatyypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,23 @@
+const accountId = 123242
+ let  accountEmail = "satyam@gmail.com"
+ var  accountPassword = "45678"
+ accountCity = "Jaipur"
+
+ let accountState 
+
+
+
+ accountEmail = "some@gmail.com"
+ accountPassword = "09876"
+ accountCity = "Ghaziabad"
+
+
+ /*
+   do not use var
+   because of issue in block and functional scope
+
+ */
+
+   console.table([accountId , accountEmail , accountPassword , accountCity])
+
+
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"  ; // treat all JS code as newer version
+
+// alert(3+3)  gives error here , we are using nodejs ,not browser
+
+// console.log(3+3)   console.log(5+7)  wrong syntax
+
+
+// numbner => 2 t0 power 53
+// bigint
+// string =>""
+// boolean => true / false 
+// null => standalone value
+// undefined =>         //when  only declared 
+// symbol => unique
+
+// object 
+ 
+let age = 18 
+let isLoggedIn = false
+let state;
+
+console.log(typeof undefined); // undefined 
+console.log(typeof null);   // object
