eec5be1bfeb5225414f39b436e0baf8fe142a136
satyamkhandani <thesatyamkhandani@gmail.com>
2025-11-08 07:45:05 +0000

    data comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,41 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 != 1);
+// console.log(2 == 1);
+
+
+
+// console.log("2" > 1) ;  //true
+// console.log("02" > 1); //true
+
+
+
+// console.log(null > 0);  //false
+// console.log(null > 0);   //false       
+// console.log(null >= 0); // true
+
+
+// console.log(undefined == 0); 
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+
+
+
+// ===  compares data type  also
+
+// console.log("2" == 2);  //true
+console.log("2" === 2); // false
+console.log("2" === "2")  
+console.log("2" === "1"); // false
+console.log(2 === 2); 
+
+
+
+// ----------------- avoid comparison betweeen diff data types
+//  write clean code
+
+
+
+
+
